ff1e976c415b10a2d3e3a7a70852d10acb91bf37
Aleksei Antipin <grauheim.today@gmail.com>
2023-03-23 19:46:19 +0700

    Homework 3

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
+let alex = new Student('Aleksei', 'male', 31);
+let shannon = new Student('Shannon', 'female', 22);
+let candy = new Student('Candy', 'female', 19);
+
+
 Student.prototype.setSubject = function (subjectName) {
-  
+	this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+	if (!(this.excluded)) {
+		this.marks.push(...marks);
+	}
 }
 
 Student.prototype.getAverage = function () {
-  
+	if (this.marks && this.marks.length > 0) {
+		return (this.marks.reduce((a, b) => a + b, 0)) / this.marks.length;
+	}
+	else {
+		return 0
+	}
 }
 
 Student.prototype.exclude = function (reason) {
-  
+	this.excluded = reason;
+	delete this.subject;
+	delete this.marks;
 }
